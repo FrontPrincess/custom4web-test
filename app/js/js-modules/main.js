f6d7fffb926187c58c0a0c50dcede687c9c84c09
Nataliya Shapovalova <natalishapov@live.com>
2017-08-02 10:36:36 +0300

    slider

--- app/js/js-modules/main.js
+++ app/js/js-modules/main.js
@@ -1,10 +1,15 @@
 //= js-assets/functions
 $(document).ready(function() {
-	$('.search-block').on('click', '.search-btn', function(event) {
-		event.preventDefault();
-		$(this).parents().find('.search-line').toggleClass('active');
-	});
+    $('.search-block').on('click', '.search-btn', function(event) {
+        event.preventDefault();
+        $(this).parents().find('.search-line').toggleClass('active');
+    });
+
+    $('.js-slick').slick({
+        // infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true
+    });
 });
-
-
-
